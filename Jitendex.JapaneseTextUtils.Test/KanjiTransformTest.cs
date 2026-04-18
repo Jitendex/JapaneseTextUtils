044371f226c4af041cea1dac6eb17e6e92fb223b
stephenmk <stephenmk@users.noreply.github.com>
2026-04-17 21:49:32 -0500

    Remove unused `using`

--- Jitendex.JapaneseTextUtils.Test/KanjiTransformTest.cs
+++ Jitendex.JapaneseTextUtils.Test/KanjiTransformTest.cs
@@ -16,8 +16,6 @@ You should have received a copy of the GNU Affero General Public License along w
 If not, see <https://www.gnu.org/licenses/>.
 */
 
-using System.Text;
-
 namespace Jitendex.JapaneseTextUtils.Test;
 
 /// <summary>
